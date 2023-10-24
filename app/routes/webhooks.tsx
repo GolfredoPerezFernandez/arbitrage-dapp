e40a3d26ef664cb4f2935ae435297048d0b4db90
Golfredo Perez Fernandez <golfredo.pf@gmail.com>
2023-10-24 17:47:06 -0400

    asd

--- app/routes/webhooks.tsx
+++ app/routes/webhooks.tsx
@@ -1,8 +1,9 @@
 import {ActionFunction, json} from '@remix-run/node';
+let count=0
 
 export const action: ActionFunction = async ({request}) => {
- 
   const payload = await request.json();
   console.log(JSON.stringify(payload))
+  console.log(count++)
   return null
 };
