a1b2ea5f4cac71bd6d104a464e59a2f74b418c8e
Golfredo Perez Fernandez <golfredo.pf@gmail.com>
2023-10-24 20:06:16 -0400

    asd

--- app/routes/webhooks.tsx
+++ app/routes/webhooks.tsx
@@ -1,7 +1,5 @@
 import {ActionFunction, json} from '@remix-run/node';
 import { parseEventData } from '~/utils/parseEventData';
-import { verifySignature } from '~/utils/verifySignature';
-import  parseUpdate  from '~/utils/parseUpdate';
 
 export const action: ActionFunction = async ({request}) => {
 
