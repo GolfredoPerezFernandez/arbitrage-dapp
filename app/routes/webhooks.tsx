69e3627a19958bd069b78ca6ebe83d3c816fe2b4
Golfredo Perez Fernandez <golfredo.pf@gmail.com>
2023-10-24 20:28:23 -0400

    asd

--- app/routes/webhooks.tsx
+++ app/routes/webhooks.tsx
@@ -1,11 +1,15 @@
 import {ActionFunction, json} from '@remix-run/node';
-import { parseEventData } from '~/utils/parseEventData';
+import { fetchTransaction } from '@wagmi/core'
 
 export const action: ActionFunction = async ({request}) => {
 
-  try {
-    console.log("data "+request);
+  try {   
+     console.log("data "+JSON.stringify(request));
 
+    const transaction = await fetchTransaction({
+      hash: '0x88ebc25f5bc02860938fc795d58a56b10a6361e57e4ef2d3c16ccb94acbf3e51',
+    })
+console.log("transaction "+transaction)
     return null
   } catch(error) {
     console.log('errorStream', error)
