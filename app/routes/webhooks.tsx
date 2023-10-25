5d2ba4fca2a3f3017720462f781fd289715605e0
Golfredo Perez Fernandez <golfredo.pf@gmail.com>
2023-10-24 20:36:07 -0400

    asd

--- app/routes/webhooks.tsx
+++ app/routes/webhooks.tsx
@@ -4,7 +4,7 @@ import { fetchTransaction } from '@wagmi/core'
 export const action: ActionFunction = async ({request}) => {
 
   try {   
-     console.log("data "+JSON.stringify(request));
+     console.log("data "+JSON.stringify(request.txs));
 
     const transaction = await fetchTransaction({
       hash: '0x88ebc25f5bc02860938fc795d58a56b10a6361e57e4ef2d3c16ccb94acbf3e51',
